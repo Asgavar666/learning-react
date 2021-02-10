4d1e1997809ee94a41b447367116b8ead34b1135
Krzysztof Badelek <k.badelek@gmail.com>
2021-02-10 18:14:05 +0100

    imageAddress

--- src/components/FAQ/FAQ.js
+++ src/components/FAQ/FAQ.js
@@ -6,7 +6,7 @@ import grayscale from './FAQ.scss';
 
 const FAQ = () => (
   <Container>
-    <Hero titleText={faqContent.title} image={faqContent.image} className={grayscale} />
+    <Hero titleText={faqContent.title} image={faqContent.imageAddress} className={grayscale} />
     <h2>{faqContent.header}</h2>
     <p>{faqContent.description}</p>
   </Container>
--- src/components/Info/Info.js
+++ src/components/Info/Info.js
@@ -5,7 +5,7 @@ import {infoContent} from '../../data/dataStore';
 
 const Info = () => (
   <Container>
-    <Hero titleText={infoContent.title} image={infoContent.image}/>  
+    <Hero titleText={infoContent.title} image={infoContent.imageAddress}/>  
     <h2>{infoContent.header}</h2>
     <p>{infoContent.description}</p>
   </Container>
